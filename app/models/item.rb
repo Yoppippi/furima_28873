d5ca429ef654539e7513198ec4346b85bba8889e
Yoppippi <yottiii0111@gmail.com>
2020-09-04 12:20:15 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :shipping_address
+  belongs_to_active_hash :until_shipping
   belongs_to :user
   has_one_attached :image
+
+  validates :item_name, :description, :category, :status, :delivery_fee, :shipping_address, :until_shipping, :price, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :delivery_fee_id, numericality: { other_than: 1 } 
+  validates :shipping_address_id, numericality: { other_than: 1 }
+  validates :until_shipping_id, numericality: { other_than: 1 }  
 end
