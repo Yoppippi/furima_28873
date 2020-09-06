c58d7172db3be4608851644e2ff10688aab07ac6
Yoppippi <yottiii0111@gmail.com>
2020-09-06 22:50:40 +0900

    非同期通信の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,5 @@ class Item < ApplicationRecord
 
   validates :image, :item_name, :description, :category, :status, :delivery_fee, :shipping_address, :until_shipping, :price, presence: true
   validates :price, format: { with: /\A[0-9]+\z/ }, inclusion: {in: 300..9999999 }
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :status_id, numericality: { other_than: 1 } 
-  validates :delivery_fee_id, numericality: { other_than: 1 } 
-  validates :shipping_address_id, numericality: { other_than: 1 }
-  validates :until_shipping_id, numericality: { other_than: 1 }  
+  validates :category_id, :status_id, :delivery_fee_id, :shipping_address_id, :until_shipping_id, numericality: { other_than: 1 } 
 end
